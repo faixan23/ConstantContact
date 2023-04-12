ca407546ac6da5e7b7dd5a6830351731e1533303
pamekar <d.pamekar@gmail.com>
2023-04-12 21:51:46 +0100

    Fix php7.4 conflict

--- src/ConstantContact/UUID.php
+++ src/ConstantContact/UUID.php
@@ -6,17 +6,16 @@ namespace PHPFUI\ConstantContact;
  * Simple UUID class for JSON output
  */
 class UUID
-	{
-	public function __construct(private string $uuid)
-		{
-		if (! \preg_match('/^[a-f\d]{8}(-[a-f\d]{4}){4}[a-f\d]{8}$/i', $uuid))
-			{
-			throw new \PHPFUI\ConstantContact\Exception\InvalidValue($uuid . ' is not a valid UUID');
-			}
-		}
+{
+    public function __construct(string $uuid)
+    {
+        if (!\preg_match('/^[a-f\d]{8}(-[a-f\d]{4}){4}[a-f\d]{8}$/i', $uuid)) {
+            throw new \PHPFUI\ConstantContact\Exception\InvalidValue($uuid . ' is not a valid UUID');
+        }
+    }
 
-	public function __toString() : string
-		{
-		return $this->uuid;
-		}
-	}
+    public function __toString(): string
+    {
+        return $this->uuid;
+    }
+}
